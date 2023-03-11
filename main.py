e8d8a6c1819a85be6ecb428a986796ddc90f0a1c
AHMEDRAZA-IT-Proffesional <m.a.r.l214786@gmail.com>
2023-03-11 21:28:17 +0500

    Added Power function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from multiplication import multiply
 from addition import add
 from subtraction import subtracT
+from exponent import power
 
 if __name__=='__main__':
      A = 6
@@ -9,3 +10,4 @@ if __name__=='__main__':
      print(add(A,B))
      print(subtracT(A,B))
      print(multiply(A,B))
+     print(power(A,B))
